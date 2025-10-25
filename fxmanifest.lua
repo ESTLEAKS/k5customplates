fe17ae034055e5dda5ca1e534e8b53041e9285db
ESTLEAKS <leaksmidnightmafia@gmail.com>
2025-10-25 01:33:55 -0400

    Add fxmanifest for Custom Plate System
    Initial setup for Custom Plate System including configuration and script files.

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'k5'
+description 'Custom Plate System for JG Mechanic'
+version '1.0.0'
+
+shared_scripts {
+    'config.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+files {
+    'plates.json'
+}
